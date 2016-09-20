108f80cf81eff5229be8ae824f62470267378ef8
Sheikh Muhammad Mustafa <mustafa@mview.com.au>
2016-09-20 12:12:30 +1000

    added opencore-amr library and configured according to requirement.

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -4,14 +4,16 @@ echo "============================================"
 echo "Updating submodules"
 git submodule update --init
 echo "============================================"
-echo "Updating libpng, expat and fribidi"
+echo "Updating libpng, expat, fribidi and opencore-amr"
 rm -rf libpng-*
 rm -rf expat-*
 rm -rf fribidi-*
 rm -rf lame-*
+rm -rf opencore-amr-*
 
-wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.21.tar.xz | tar xJ
+wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.25.tar.xz | tar xJ
 wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz | tar xz
 wget -O- http://fribidi.org/download/fribidi-0.19.7.tar.bz2 | tar xj
 wget -O- http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz | tar xz
+wget -O- http://sourceforge.net/projects/opencore-amr/files/opencore-amr/opencore-amr-0.1.3.tar.gz | tar xz
 echo "============================================"
